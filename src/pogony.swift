88f0b142b0ab7bec72fcffa8e9698562bd006bbf
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-06-11 01:50:08 +0300

    Update pogony.swift

--- src/pogony.swift
+++ src/pogony.swift
@@ -31,6 +31,7 @@ public class Pogony{
         "Accept-Encoding":"deflate, zstd",
         "Accept-Language":"en-US,en;q=0.9",
         "Referer":"https://pogony.org/registry",
+        "host":"pogony.org",
         "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36"
         ]
 
